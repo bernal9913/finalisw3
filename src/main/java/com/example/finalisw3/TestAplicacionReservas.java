c0fa6e3330536c5fef67a79e82c2e9321f4090a1
bernal9913 <ramirez.bernal.jesus1@gmail.com>
2023-11-11 23:12:13 -0700

    creacion y setteo de junit pd: jalan y no jalan

---
+++ src/main/java/com/example/finalisw3/TestAplicacionReservas.java
@@ -0,0 +1,20 @@
+package com.example.finalisw3;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestAplicacionReservas {
+    private AplicacionReservas aplicacionReservas;
+
+    @BeforeEach
+    public void setUp() {
+        aplicacionReservas = new AplicacionReservas();
+    }
+
+    @Test
+    public void hacerReservaAgregaReserva() {
+        aplicacionReservas.hacerReserva("2023-12-01", "18:00", 4);
+        assertEquals(1, aplicacionReservas.listarReservas().size());
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.finalisw3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.junit.jupiter.api;
 
 
     opens com.example.finalisw3 to javafx.fxml;
