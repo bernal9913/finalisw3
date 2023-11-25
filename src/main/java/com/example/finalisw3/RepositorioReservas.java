031cda67b6fd89058c30969266542349637964f0
Carlos Bernal <78837548+bernal9913@users.noreply.github.com>
2023-11-25 14:07:53 -0700

    fragment de codigo para leer y guardar csv (no funca)

--- src/main/java/com/example/finalisw3/RepositorioReservas.java
+++ src/main/java/com/example/finalisw3/RepositorioReservas.java
@@ -1,13 +1,17 @@
 package com.example.finalisw3;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioReservas {
     private List<Reserva> reservas;
+    private String archivoCSV;
 
-    public RepositorioReservas() {
+    public RepositorioReservas(/* String archivoCSV */) {
         this.reservas = new ArrayList<>();
+        //this.archivoCSV = archivoCSV;
+        //cargarReservas();
     }
 
     public void agregarReserva(Reserva reserva) {
@@ -34,4 +38,34 @@ public class RepositorioReservas {
             System.out.println("Ha ocurrido un error agregando la reserva");
         }
     }
+    /**
+     * Este fragmento de cargar reservas es una duda enorme si jala
+     * pero fue agregado por si a lo mejor se necesitaba guardar en csv
+     * usar bajo su propio riesgo
+     *
+     * **/
+    private void cargarReservas(){
+        try(BufferedReader br = new BufferedReader(new FileReader(archivoCSV))){
+            String line;
+            while((line = br.readLine()) != null){
+                String [] partes = line.split(",");
+                if(partes.length == 3){
+                    Reserva reserva = new Reserva(partes[0], partes[1], Integer.parseInt(partes[2]), partes[3]);
+                    reservas.add(reserva);
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private void guardarReserva(){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivoCSV))){
+            for (Reserva reserva : reservas){
+                bw.write(String.format("%s,%s,%d%n", reserva.getFecha(), reserva.getHora(), reserva.getComensales()));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
